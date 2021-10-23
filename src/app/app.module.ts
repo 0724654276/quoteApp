6000302a3ff8fb5d711768ffa39bce36ae8e3cc4
0724654276 <you@example.com>
2021-10-23 18:06:02 +0300

    put app module selector into app component html for view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
-import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
-
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { FormsModule } from '@angular/forms';
+import { DateCountPipe } from './date-count.pipe';
+import { DatePassedPipe } from './date-passed.pipe';
+import { HighlightDirective } from './highlight.directive';
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
+    QuoteFormComponent,
     QuoteDetailComponent,
-    QuoteFormComponent
+    DateCountPipe,
+    DatePassedPipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
